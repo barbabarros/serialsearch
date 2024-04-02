00253bd7e727f3cdd9728eb0190a08ffb9551097
barbabarros <40841971+barbabarros@users.noreply.github.com>
2024-04-02 04:33:47 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,69 +9,83 @@ struct teste {
     string nome;
 };
 
-void leitura (teste t[]) {
+/*void leitura (teste t[]) {
     for (int i = 0; i < 5; i++) {
         cout << "Digite o codigo: ";
         cin >> t[i].codigo;
         cout << "Digite o nome: ";
         cin >> t[i].nome;
     }
-}
+}*/
 
 void serial(teste t[]) {
     int busca;
-    cout << "Qual o codigo? ";
+    cout << "Qual o codigo? for(vazio) ";
     cin >> busca;
     auto inicio = chrono::high_resolution_clock::now();
+
     int i = 0;
-    for (; busca > t[i].codigo; i++) {}
+    for (; busca > t[i].codigo && i < 10; i++) {}
     if (busca == t[i].codigo) {
-        cout << "for(vazio) ACHEI! Codigo: " << t[i].codigo << " Nome: " << t[i].nome << endl;
-    } else cout << "for(vazio) NAO ACHEI!" << endl;
+        cout << "for(vazio) " << i+1 << "x" << " Codigo: " << t[i].codigo << " Nome: " << t[i].nome << endl << endl;
+    }
+
     auto final = chrono::high_resolution_clock::now();
     auto tempo = chrono::duration_cast<chrono::microseconds>(final - inicio);
-    cout << "Tempo decorrido: " << tempo.count() << " microssegundos" << endl;
+    cout << "Tempo decorrido: " << tempo.count() << " microssegundos" << endl << endl;
 }
 
 void serial1(teste t[]) {
     int busca;
-    cout << "Qual o codigo? ";
+    cout << "Qual o codigo? for() ";
     cin >> busca;
     auto inicio = chrono::high_resolution_clock::now();
+
     int i = 0;
-    for (; busca >= t[i].codigo; i++) {
+    for (; busca >= t[i].codigo && i < 10; i++) {
         if (busca == t[i].codigo) {
-            cout << "for() ACHEI! Codigo: " << t[i].codigo << " Nome: " << t[i].nome << endl;
-        } else cout << "for() NAO ACHEI!" << endl;  
+            cout << "for() " << i+1 << "x" << " Codigo: " << t[i].codigo << " Nome: " << t[i].nome << endl << endl;
+        }
     }
+
     auto final = chrono::high_resolution_clock::now();
     auto tempo = chrono::duration_cast<chrono::microseconds>(final - inicio);
-    cout << "Tempo decorrido: " << tempo.count() << " microssegundos" << endl;
+    cout << "Tempo decorrido: " << tempo.count() << " microssegundos" << endl << endl;
 }
 
 void serial2(teste t[]) {
     int busca;
-    cout << "Qual o codigo? ";
+    cout << "Qual o codigo? while() ";
     cin >> busca;
     auto inicio = chrono::high_resolution_clock::now();
+
     int i = 0;
-    while (busca > t[i].codigo) {
-        i++;
-    }
+    while (busca > t[i].codigo && i < 10) i++;
     if (busca == t[i].codigo) {
-        cout << "while() ACHEI! Codigo: " << t[i].codigo << " Nome: " << t[i].nome << endl;
-    } else cout << "while() NAO ACHEI!" << endl;
+        cout << "while() " << i+1 << "x" << " Codigo: " << t[i].codigo << " Nome: " << t[i].nome << endl << endl;
+    }
+
     auto final = chrono::high_resolution_clock::now();
     auto tempo = chrono::duration_cast<chrono::microseconds>(final - inicio);
-    cout << "Tempo decorrido: " << tempo.count() << " microssegundos" << endl;
+    cout << "Tempo decorrido: " << tempo.count() << " microssegundos" << endl << endl;
 }
 
 
 int main() {
-    teste t[10];
-    leitura(t);
+    teste t[10] = {
+        {2,"aa"},
+        {3,"aa"},
+        {4,"aa"},
+        {5,"aa"},
+        {8,"aa"},
+        {11,"aa"},
+        {21,"aa"},
+        {22,"aa"},
+        {330,"aa"},
+        {5600,"aa"},
+    };
+    //leitura(t);
     serial(t);
     serial1(t);
     serial2(t);
-    
 }
